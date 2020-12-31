2c746628ea4bcdf85714b68c57130d8912df82ac
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2020-12-31 14:50:30 +0900

    flag.StringVarの基本

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
-	var name = flag.String(
+	var name string
+	flag.StringVar(
+		&name,                          // flagのValueを格納する変数
 		"name",                         // name（設定するflag name）
 		"Tom",                          // value（flagが指定されなかった場合のdefault value）
 		"Please specify -name <Value>", // usage（使用方法説明）
 	)
 	flag.Parse()
-	fmt.Println("The name flag is", *name)
+	fmt.Println("The name flag is", name)
 }
