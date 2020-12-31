b37d8be41ab3b84fffc7e4b6a86a5b4a7b639408
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2020-12-31 15:50:16 +0900

    NewFlagSetの基本

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var name string
-	flag.StringVar(
-		&name,                          // flagのValueを格納する変数
-		"name",                         // name（設定するflag name）
-		"Tom",                          // value（flagが指定されなかった場合のdefault value）
-		"Please specify -name <Value>", // usage（使用方法説明）
+	flags := flag.NewFlagSet("sub", flag.ContinueOnError)
+	filePath := flags.String(
+		"f",            // name（設定するflag name）
+		"/tmp/",        // value（flagが指定されなかった場合のdefault value）
+		"path to file", // usage（使用方法説明）
 	)
-	flag.Parse()
-	fmt.Println("The name flag is", name)
+
+	flags.Parse(os.Args[2:])
+	fmt.Println("file path ", *filePath)
 }
